8f546dbecde843ae8a871d766c364d9f2c3d19f7
VlademirMelo <vlademir.melo@globalsys.com.br>
2022-09-03 19:39:00 -0300

    feature: marcar exames

--- primeiro-projeto/src/app/app-routing.module.ts
+++ primeiro-projeto/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationModule } from './authentication/authentication.module';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
-
+  { path: 'appointment', component: AppointmentComponent},
   { path: '**', redirectTo: ''}
 
 ];
