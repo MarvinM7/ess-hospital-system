2c2e38c2395d3484782dd3e728de5bcad1b2dc86
julia <juliaqmachado@gmail.com>
2022-09-03 23:43:58 -0300

    atualização das rotas para contatos, quem somos

--- primeiro-projeto/src/app/app-routing.module.ts
+++ primeiro-projeto/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { AuthenticationModule } from './authentication/authentication.module';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppointmentComponent } from './appointment/appointment.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
+  { path: 'contato', component: ContactComponent},
+  { path: 'quemsomos', component: AboutUsComponent},
   { path: 'appointment', component: AppointmentComponent},
   { path: '**', redirectTo: ''}
 
