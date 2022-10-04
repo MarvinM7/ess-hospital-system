98bf33d45ad11f7d2c994b4123dd6ef987d73a87
ksbs22 <ksbs@cin.ufpe.br>
2022-10-04 10:18:44 -0300

    correçao de tela/testes de serviço

--- primeiro-projeto/testAppointment.py
+++ primeiro-projeto/testAppointment.py
@@ -6,18 +6,17 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
 
 
 servico = Service(ChromeDriverManager().install())
 chrome_opt = Options()
 chrome_opt.add_experimental_option("detach", True)
 navegador = webdriver.Chrome(service=servico, options=chrome_opt)
-#alert = Alert(navegador)
 
 navegador.get('http://localhost:4200')
-
 navegador.find_element('xpath', '//*[@id="navbarCollapse"]/ul/li[2]/a').click()
-navegador.find_element('xpath', '//*[@id="cpf"]').send_keys('12345678990')
+navegador.find_element('xpath', '//*[@id="email"]').send_keys('tes@email.com')
 navegador.find_element('xpath', '//*[@id="password"]').send_keys('123456')
 navegador.find_element('xpath', '/html/body/app-root/app-login/html/body/section/div/div/div/div/div/form/div[4]/button').click()
 try: 
@@ -26,6 +25,11 @@ try:
     alert.accept()
 except TimeoutException:
     print()
-#navegador.switch_to.window('main')
-navegador.find_element('xpath', '//*[@id="navbarCollapse"]/ul/li[3]/a').click()
-#navegador.find_element('xpath', '/html/body/app-root/app-login/html/body/section/div/div/div/div/div/form/div[5]/a').click()
+
+
+navegador.find_element('xpath', '/html/body/app-root/app-homepage/main/div[2]/a[1]').send_keys('\n')
+select = Select(navegador.find_element('xpath', '/html/body/app-root/app-schedule-appointment/html/body/section/div/div[1]/div/div/div/form/div[2]/select'))
+select.select_by_visible_text('Psiquiatria')
+navegador.find_element('xpath', '//*[@id="local"]').send_keys('Recife')
+navegador.find_element('xpath', '/html/body/app-root/app-schedule-appointment/html/body/section/div/div[1]/div/div/div/form/div[4]/input').send_keys('01112022')
+navegador.find_element('xpath', '/html/body/app-root/app-schedule-appointment/html/body/section/div/div[1]/div/div/div/form/div[5]/button').click()
