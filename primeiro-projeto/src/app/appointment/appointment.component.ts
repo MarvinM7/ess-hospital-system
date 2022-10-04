e93f333841fce0d6016eed87373e540906224206
Marvin Miguel <marvin.dev12@gmail.com>
2022-10-04 07:08:17 -0300

    add lista e editar exame na pagina

--- primeiro-projeto/src/app/appointment/appointment.component.ts
+++ primeiro-projeto/src/app/appointment/appointment.component.ts
@@ -9,8 +9,11 @@ import { AppointmentService } from '../services/appointment.service';
 })
 export class AppointmentComponent implements OnInit {
   mostrarModal: boolean = false
+  appointments: any = [];
   
-  constructor(private appointmentService: AppointmentService, private router: Router){}
+  constructor(private appointmentService: AppointmentService, private router: Router) {
+    this.listarExame();
+  }
   
   ngOnInit(): void {
   }
@@ -28,7 +31,32 @@ export class AppointmentComponent implements OnInit {
         local,
         dataExame
       )
+      await this.listarExame();
+    } catch(error){
+      alert(error)
+    }
+  }
 
+  async listarExame() {
+    try {
+      const data: any = [];
+      (await this.appointmentService.listarExame())
+      .subscribe((snapshots) => snapshots.forEach((snapshot: any) => {
+        data.push({
+          id: snapshot.id,
+          ...snapshot.data(),
+        })
+      }));
+      this.appointments = data ?? [];
+    } catch(error){
+      alert(error)
+    }
+  }
+
+  async excluirExame(id: string) {
+    try {
+      await this.appointmentService.excluirExame(id);
+      await this.listarExame();
     } catch(error){
       alert(error)
     }
